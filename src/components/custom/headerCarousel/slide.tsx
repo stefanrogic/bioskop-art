c5d60506ac6add959782f5564adc27eeffe9aca1
stefanrogc <stefanrogic@protonmail.com>
2024-05-09 17:56:05 +0200

    changed slide design

--- src/components/custom/headerCarousel/slide.tsx
+++ src/components/custom/headerCarousel/slide.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import { cva, type VariantProps } from "class-variance-authority";
 import { SwiperSlideProps } from "swiper/react";
 import { cn } from "@/lib/utils";
-
-import Image from "next/image";
 
 const headerCarouselSlideVariants = cva("relative cursor-pointer");
 
@@ -14,8 +15,8 @@ export interface HeaderCarouselSlideProps extends React.HTMLAttributes<SwiperSli
 
 function HeaderCarouselSlide({ className, slideName, slideDate, slideUrl }: HeaderCarouselSlideProps): JSX.Element {
   return (
-    <a className={cn(headerCarouselSlideVariants(), className)} href="#">
-      <div className="absolute z-50 flex flex-col justify-center min-[900px]:items-start left-5 min-[900px]:left-20 bottom-14 min-[900px]:bottom-32 w-full">
+    <Link href="/" className={cn(headerCarouselSlideVariants(), className)}>
+      <div className="absolute z-50 flex flex-col items-start left-20 bottom-24 w-full">
         <div className="flex space-x-3 mb-3 min-[900px]:me-auto items-center">
           <div className="px-2 pt-3 pb-2 bg-red-700 text-white text-sm leading-3 font-bold rounded-sm">Sala 1</div>
           <div className="px-2 pt-3 pb-2 bg-red-700 text-white text-sm leading-3 font-bold rounded-sm">3D</div>
@@ -27,7 +28,7 @@ function HeaderCarouselSlide({ className, slideName, slideDate, slideUrl }: Head
       <div className="absolute top-0 bg-gradient-to-b from-black to-transparent opacity-25 w-full h-24"></div>
       <Image className="object-cover w-full h-full mx-auto" src={slideUrl} alt="slide-img" priority />
       <div className="absolute bottom-0 bg-gradient-to-t from-black opacity-45 w-full h-1/2"></div>
-    </a>
+    </Link>
   );
 }
 export { HeaderCarouselSlide, headerCarouselSlideVariants };
