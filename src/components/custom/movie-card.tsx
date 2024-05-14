b9f3ba27c40994a7f67bacd46e35bf59127203c7
rasgrin <stefanrogic@protonmail.com>
2024-05-14 13:34:09 +0200

    new design and responsivness for repertoar section

--- src/components/custom/movie-card.tsx
+++ src/components/custom/movie-card.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 interface Data {
@@ -6,13 +7,16 @@ interface Data {
     date: string;
     tag?: string;
     tagColor?: string;
+    img: any;
   };
 }
 
 function MovieCard({ data }: Data): JSX.Element {
   return (
     <Link href="#" className="w-full flex flex-col gap-5">
-      <div className="w-full aspect-[3/4] bg-slate-400"></div>
+      <div className="w-full aspect-[3/4] bg-slate-400">
+        <Image src={data?.img} alt="movie-poster" className="object-cover w-full h-full" />
+      </div>
       <div className="flex flex-col gap-7">
         <div className="flex flex-col">
           {data?.name && <h4 className="text-xl font-semibold">{data?.name}</h4>}
