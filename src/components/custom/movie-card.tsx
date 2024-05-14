a277e3d1b6d4c7a810a5cb34a9e7739df78b6faa
rasgrin <stefanrogic@protonmail.com>
2024-05-14 11:54:21 +0200

    added repertoar section to homepage

---
+++ src/components/custom/movie-card.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface MovieData {
+  data: object;
+}
+
+function MovieCard({ data }: MovieData): JSX.Element {
+  return (
+    <Link href="#" className="w-full flex flex-col gap-5">
+      <div className="w-full aspect-[3/4] bg-slate-400"></div>
+      <div className="flex flex-col gap-7">
+        <div className="flex flex-col">
+          {data?.name && <h4 className="text-xl font-semibold">{data?.name}</h4>}
+          {data?.date && <p className="text-light text-md">{data?.date}</p>}
+        </div>
+        {data?.tag && <div className={`px-5 py-2 ${data?.tagColor} text-white font-bold text-sm me-auto`}>{data?.tag}</div>}
+      </div>
+    </Link>
+  );
+}
+export { MovieCard };
