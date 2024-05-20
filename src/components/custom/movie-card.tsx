a87073fb4f6e8e7082a3b2379678d28bd33d6924
stefanrogc <stefanrogic@protonmail.com>
2024-05-20 11:22:17 +0200

    added genre on cards when hovering

--- src/components/custom/movie-card.tsx
+++ src/components/custom/movie-card.tsx
@@ -14,7 +14,11 @@ interface Data {
 function MovieCard({ data }: Data): JSX.Element {
   return (
     <Link href="#" className="w-full flex flex-col gap-5">
-      <div className="w-full aspect-[3/4] bg-slate-400">
+      <div className="relative w-full aspect-[3/4] bg-slate-400">
+        <div className="opacity-0 hover:opacity-100 absolute flex flex-col justify-center items-center gap-1 w-full h-full bg-black text-white font-bold bg-opacity-30">
+          <span>Akcija</span>
+          <span>Drama</span>
+        </div>
         <Image src={data?.img} alt="movie-poster" className="object-cover w-full h-full" />
       </div>
       <div className="flex flex-col gap-7">
--- src/containers/home/repertoar.tsx
+++ src/containers/home/repertoar.tsx
@@ -16,7 +16,7 @@ function getCurrentMovies() {
 
 function RepertoarContainer(): JSX.Element {
   return (
-    <section className="min-h-[50vh] flex flex-col items-start gap-10 px-5 lg:px-20 mt-40">
+    <section className="min-h-[50vh] flex flex-col items-start gap-10 px-5 lg:px-20 mt-20">
       <div className="flex flex-col">
         <h3 className="font-bold text-xl text-red-500">TRENUTNO NA</h3>
         <h1 className="font-bold text-4xl">REPERTOARU</h1>
