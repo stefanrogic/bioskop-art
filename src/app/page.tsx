35b72e4a277dbd09e78ff89bc7919a069c417338
stefanrogc <stefanrogic@protonmail.com>
2024-05-21 11:02:57 +0200

    fixed error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import { Footer } from "@/containers/footer";
 import { BlogContainer } from "@/containers/home/blog";
 import { ContactContainer } from "@/containers/home/contact";
 import { Header } from "@/containers/home/header";
-import { RepertoarContainer } from "@/containers/home/repertoar";
 
 export default function HomePage(): JSX.Element {
   return (
     <>
       <Header />
-      {/* <RepertoarContainer /> */}
+
       <BlogContainer />
       <ContactContainer />
       <Footer />
